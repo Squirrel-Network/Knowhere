26d77aee5533df257be0d8fb177486df56cc4f57
briosheje <uaesbriosheje@gmail.com>
2021-02-27 01:19:40 +0100

    feat: theme persistence
    Added theme persistence through either LocalStorage or SessionStorage.
    IndexedDB was intentionally avoided since either of above are enough safe and supported.
    
    Minor refactor on App.vue to provide a single entry file on src/service in order to simplify maintenance against import name changes.

---
+++ src/service/PersistThemePlugin.ts
@@ -0,0 +1,134 @@
+import Vue from 'vue';
+
+declare module 'vue/types/vue' {
+	interface Vue {
+		persistThemeService: PersistThemePlugin
+	}
+}
+
+type BanError =
+	{ error: String
+  }
+  
+export type ThemeOptions = 
+  {
+    isDarkTheme?: boolean;
+  }
+
+
+export enum PersistThemePluginOptionsStoreType 
+{
+  LOCALSTORAGE,
+  SESSIONSTORAGE
+}
+  
+export type PersistThemePluginOptions =
+  {
+    storeType: PersistThemePluginOptionsStoreType
+  }
+
+export default class PersistThemePlugin {
+  private static STORAGE_THEME_OPTIONS_KEY = 'theme-opts';
+
+	private constructor(options: PersistThemePluginOptions) {
+    this.options = options;
+    this.storage = this.isLocalStorage ?
+      window.localStorage :
+      window.sessionStorage;
+
+    this.loadOptions();
+  }
+
+  /**
+   * Holds the instance of the preferred storage.
+   *
+   * @protected
+   * @type {Storage}
+   * @memberof PersistThemePlugin
+   */
+  protected storage: Storage;
+  
+  /**
+   * Holds the instance of the current theme options.
+   *
+   * @protected
+   * @type {ThemeOptions}
+   * @memberof PersistThemePlugin
+   */
+  protected themeOptions: ThemeOptions = {};
+  
+  /**
+   * Holds the options assigned in the install static method.
+   *
+   * @see install
+   * @type {PersistThemePluginOptions}
+   * @memberof PersistThemePlugin
+   */
+  protected options: PersistThemePluginOptions;
+
+  /**
+   * Loads the current options from the configured underlying storage.
+   *
+   * @protected
+   * @memberof PersistThemePlugin
+   */
+  protected loadOptions(): void {
+    const opts = this.storage.getItem(PersistThemePlugin.STORAGE_THEME_OPTIONS_KEY);
+    if (opts) {
+      this.themeOptions = JSON.parse(opts);
+    } else {
+      this.themeOptions = {};
+    }
+  }
+
+  /**
+   * Establishes whether the plugin was configured to use the local storage as a store.
+   *
+   * @readonly
+   * @memberof PersistThemePlugin
+   */
+  public get isLocalStorage() {
+    return this.options.storeType === PersistThemePluginOptionsStoreType.LOCALSTORAGE;
+  }
+
+  /**
+   * Establishes whether the plugin was configured to use the session storage as a store.
+   *
+   * @readonly
+   * @memberof PersistThemePlugin
+   */
+  public get isSessionStorage() {
+    return this.options.storeType === PersistThemePluginOptionsStoreType.SESSIONSTORAGE;
+  }
+
+  /**
+   * Returns a copy of the current theme options.
+   *
+   * @returns {ThemeOptions}
+   * @memberof PersistThemePlugin
+   */
+  public get currentOptions() : ThemeOptions {
+    return this.themeOptions;
+  }
+
+  /**
+   * Updates and persists the curren options on the configured underlying storage.
+   *
+   * @param {ThemeOptions} [opts={}]
+   * @memberof PersistThemePlugin
+   */
+  public updateOptions(opts: ThemeOptions = {}) : void {
+    this.themeOptions = {
+      ...this.themeOptions,
+      ...opts
+    };
+    this.storage.setItem(PersistThemePlugin.STORAGE_THEME_OPTIONS_KEY, JSON.stringify(this.themeOptions));
+    this.loadOptions();
+  }
+
+	public static install(Vue: any, options: PersistThemePluginOptions) {
+    const _instance = new PersistThemePlugin(options);
+    _instance.options = options;
+    Vue.prototype.persistThemeService = _instance;
+  }
+}
--- vue.config.js
+++ vue.config.js
@@ -14,5 +14,13 @@ module.exports = {
 
 				return args;
 			});
+
+		// Prevents safari caching
+		if (process.env.NODE_ENV === 'development') {
+			config.plugins.delete('preload')
+			config.output
+				.filename('[name].[hash].js') 
+				.end() 
+		}
 	}
 }
